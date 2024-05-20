033efe44a0a22d40b704dc54d6aa0e6ebbdf8f51
Allan Rubio <rubio_allan@yahoo.com>
2024-05-19 23:52:48 -0700

    api routes mostly working, need to test delete route

---
+++ server/db.js
@@ -0,0 +1,120 @@
+// Data Layer
+const pg = require("pg");
+const bcrypt = require("bcrypt");
+const { v4: uuidv4 } = require("uuid");
+
+const client = new pg.Client(
+  process.env.DATABASE_URL || { database: "the_acme_store" }
+);
+
+const createTables = () => {
+  const SQL = `
+          DROP TABLE IF EXISTS favorite;
+          DROP TABLE IF EXISTS product;
+          DROP TABLE IF EXISTS "user";
+  
+          CREATE TABLE "user" (
+              id UUID PRIMARY KEY,
+              username VARCHAR(100) NOT NULL UNIQUE,
+              password VARCHAR(255) NOT NULL
+          );
+          CREATE TABLE product (
+              id UUID PRIMARY KEY,
+              name VARCHAR(100) NOT NULL
+          );
+          CREATE TABLE favorite (
+              id UUID PRIMARY KEY,
+              product_id UUID REFERENCES product(id) NOT NULL,
+              user_id UUID REFERENCES "user"(id) NOT NULL,
+              CONSTRAINT user_fav_product UNIQUE (user_id, product_id)
+              )
+          `;
+  return client.query(SQL);
+};
+
+// username and password are being desctructured
+const createUser = async ({ username, password }) => {
+  password = await bcrypt.hash(password, 5);
+
+  const SQL = `
+          INSERT INTO "user" (id, username, password)
+          VALUES ($1, $2, $3) 
+          RETURNING *
+      `;
+  return client.query(SQL, [uuidv4(), username, password]);
+};
+
+const createProduct = async (name) => {
+  const SQL = `
+          INSERT INTO product (id, name)
+          VALUES ($1, $2) 
+          RETURNING *
+      `;
+  return client.query(SQL, [uuidv4(), name]);
+};
+
+const createFavorite = async ({ user_id, product_id }) => {
+  const SQL = `
+          INSERT INTO favorite (id, user_id, product_id)
+          VALUES ($1, $2, $3) 
+          RETURNING *
+      `;
+  return client.query(SQL, [uuidv4(), user_id, product_id]);
+};
+
+const fetchUsers = async () => {
+  const SQL = `
+    SELECT id, username FROM "user"
+    `;
+
+  const results = await client.query(SQL);
+  return results.rows;
+};
+
+const fetchProducts = async () => {
+  const SQL = `
+    SELECT * FROM product
+    `;
+
+  const results = await client.query(SQL);
+  return results.rows;
+};
+
+// need to figure out join better
+const fetchFavorites = async (userId) => {
+  const SQL = `
+    SELECT 
+        favorite.id AS favorite_id,
+        user_id,
+        username,
+        product_id,
+        name AS product_name
+    FROM favorite
+    JOIN "user" ON favorite.user_id = "user".id
+    JOIN product ON favorite.product_id = product.id
+    WHERE user_id = $1;
+`;
+  const results = await client.query(SQL, [userId]);
+  return results.rows;
+};
+
+const deleteFavorite = async ({ user_id, product_id }) => {
+  const SQL = ` 
+    DELETE 
+    FROM favorite
+    WHERE user_id = $1 AND product_id = $2`;
+
+  await client.query(SQL, [user_id, product_id]);
+};
+
+module.exports = {
+  client,
+  createTables,
+  createUser,
+  createProduct,
+  createFavorite,
+  fetchUsers,
+  fetchProducts,
+  fetchFavorites,
+  deleteFavorite,
+};
---
+++ server/index.js
@@ -0,0 +1,126 @@
+// Express Application // API Routes // Init Function
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+const {
+  client,
+  createTables,
+  createUser,
+  createProduct,
+  fetchUsers,
+  fetchProducts,
+  createFavorite,
+  fetchFavorites,
+  deleteFavorite,
+} = require("./db");
+
+// POST // Create
+app.post("/api/users/:id/favorites", async (req, res, next) => {
+  try {
+    res.status(201).send(
+      await createFavorite({
+        user_id: req.params.id,
+        product_id: req.body.product_id,
+      })
+    );
+  } catch (error) {
+    next(error);
+  }
+});
+// GET // Read // Users
+app.get("/api/users", async (req, res, next) => {
+  try {
+    res.send(await fetchUsers());
+  } catch (ex) {
+    next(ex);
+  }
+});
+// GET // Read // Products
+app.get("/api/products", async (req, res, next) => {
+  try {
+    res.send(await fetchProducts());
+  } catch (error) {
+    next(error);
+  }
+});
+// GET // Read // User - Favorites
+app.get("/api/users/:id/favorites", async (req, res, next) => {
+  try {
+    res.send(await fetchFavorites(req.params.id));
+  } catch (error) {
+    next(error);
+  }
+});
+// Delete
+app.delete("/api/users/:userId/favorites/:id", async (req, res, next) => {
+  try {
+    await deleteFavorite({
+      user_id: req.params.userId,
+      product_id: req.params.id,
+    });
+    res.sendStatus(204);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+// Init Function
+const init = async () => {
+  // get client by req from db.js and connect
+  await client.connect();
+  console.log("connected");
+
+  await createTables();
+  console.log("tables created");
+
+  // Users and Passwords // desctructured
+  await Promise.all([
+    createUser({ username: "Steve", password: "stevestevesteve" }), // user 0
+    createUser({ username: "Tim", password: "nottimcook" }), // user 1
+    createUser({ username: "Jimmy", password: "itsjimmyyyy" }), // user 2
+
+    // Products Available
+    createProduct("MacBook Pro"), // 0
+    createProduct("Samsung 4K TV"), // 1
+    createProduct("Vaccum"), // 2
+    createProduct("Toothbrush"), // 3
+  ]);
+
+  console.log("data seeded");
+
+  // Fetch users and products
+  const users = await fetchUsers();
+  const products = await fetchProducts();
+
+  const steve = users.find((user) => user.username === "Steve");
+  const tim = users.find((user) => user.username === "Tim");
+  const jimmy = users.find((user) => user.username === "Jimmy");
+
+  // Create User Skills
+  await Promise.all([
+    createFavorite({ user_id: steve.id, product_id: products[0].id }),
+    createFavorite({ user_id: steve.id, product_id: products[1].id }),
+
+    createFavorite({ user_id: tim.id, product_id: products[1].id }),
+    createFavorite({ user_id: tim.id, product_id: products[2].id }),
+
+    createFavorite({ user_id: jimmy.id, product_id: products[2].id }),
+    createFavorite({ user_id: jimmy.id, product_id: products[3].id }),
+  ]);
+
+  console.log("User Favorites seeded");
+
+  const userFavorites = await fetchFavorites(steve.id);
+  console.log("User Favorites: ", userFavorites);
+
+  //   await deleteFavorite(userFavorites[0].id);
+
+  //   const userFavoritesAfterDelete = await fetchFavorites(steve.id);
+  //   console.log({ userFavoritesAfterDelete });
+
+  app.listen(port, () => console.log(`listening on port ${port}`));
+};
+
+init();
